e73c1cbbeeaf44a8eba2e041ef1f6a7ec071b9ec
Siskosee <seeskosee@seeskoseesko.com>
2024-02-10 05:57:22 -0800

    console

--- console.py
+++ console.py
@@ -14,6 +14,8 @@ class HBNBCommand(cmd.Cmd):
     HBNBCommand class provides a command-line interface.
     """
 
+    intro = ("Welcome to the HBNB Command Line Interface.\n"
+             "Type 'help' for more commands.\n")
     prompt = "(hbnb) "
 
     class_mapping = {'BaseModel': BaseModel, 'User': User}
